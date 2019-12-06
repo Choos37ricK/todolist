6cb7b1688f9190948bc40804e14f4d907e89a0b5
Girin Ilya <39518953+Choos37ricK@users.noreply.github.com>
2019-12-06 19:45:17 +0300

    date check released

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -1,7 +1,20 @@
 $(function() {
 
+function isValid(str) {
+    var reg = new RegExp("(19|20)\\d\\d-((0[1-9]|1[012])-(0[1-9]|[12]\\d)|(0[13-9]|1[012])-30|(0[13578]|1[02])-31)");
+    return reg.test(str);
+}
+
 $('#save-task').click(function() {
     var data = $('#task-form form').serialize();
+
+    var date = document.forms[0].elements[1].value;
+    if (!isValid(date)) {
+        alert('Введите дату в верном формате!');
+        return false;
+    }
+
+
 
     $.ajax({
         method: "POST",
@@ -15,6 +28,8 @@ $('#save-task').click(function() {
             task.id = response;
 
             var dataArray = $('#task-form form').serializeArray();
+
+
 
             for(i in dataArray) {
                 task[dataArray[i]['name']] = dataArray[i]['value'];
